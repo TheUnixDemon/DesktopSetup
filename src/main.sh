895b0adc8c8ad41e3d551c92bf103716b0ca93f4
TheUnixDaemon <theunixdaemon@gmail.com>
2025-07-19 23:04:21 +0200

    finished until first testing phase

--- src/main.sh
+++ src/main.sh
@@ -3,6 +3,5 @@
 # env variables
 export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # path of source
 
-# setup package requirements 
-source "$ROOT_DIR/inst.sh"
-
+source "$ROOT_DIR/inst.sh" # setup package requirements 
+source "$ROOT_DIR/rsync.sh" # copy backup files
