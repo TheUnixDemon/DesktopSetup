b451fdeba14007c90d4daa977824d8a0589db576
TheUnixDemon <theunixdaemon@gmail.com>
2025-07-19 22:24:55 +0200

    installation script finished

---
+++ src/inst.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# packages
+pkgs_dir="$ROOT_DIR/packages"
+
+# pacman pkgs
+pacman_pkgs_dir="$pkgs_dir/pacman"
+nvidia_pkgs="$pacman_pkgs_dir/nvidia.txt"
+amd_pkgs="$pacman_pkgs_dir/amd.txt"
+pacman_pkgs="$pacman_pkgs_dir/pkgs.txt"
+
+# yay
+yay_pkgs_dir="$pkgs_dir/yay"
+yay_pkgs="$yay_pkgs_dir/pkgs.txt"
+
+# flatpak
+flatpak_pkgs_dir="$pkgs_dir/flatpak"
+flatpak_pkgs="$flatpak_pkgs_dir/pkgs.txt"
+
+# modules
+modules_list="$pkgs_dir/modules.txt"
+
+# checking if package file exits
+check_pkgs() {
+    for pkgs_file in "$@"; do
+        echo "loading pkg file *$pkgs_file* ..."
+        if [[ ! -f "$pkgs_file" ]]; then
+            echo "*$pkgs_file* not found; abort installation"
+            return 1
+        fi
+    done
+    return 0
+}
+
+# installs packages with pacman (offical pkgs)
+inst_pacman_pkgs() {
+    echo "installaing packages via Pacman (offical)"
+    if check_pkgs "$@"; then # checking if pkgs file is existing
+        # installing pkgs
+        pkgs=$(grep -hvE "^\s*#|^\s*$" "$@")
+        if [[ -n "$pkgs" ]]; then
+            echo -e "following packages will be installed ...\n$pkgs"
+            sudo pacman -Syu --needed $pkgs && echo "installation successfully"; return 0
+            echo "installation failed"
+        else
+            echo "pkg files are empty; abort installation"
+        fi
+    fi
+    return 1
+}
+
+# installs packages with yay (aur pkgs)
+inst_yay_pkgs() {
+    echo "installaing packages via Yay (aur)"
+    if check_pkgs "$@"; then # checking if pkgs file is existing
+        # installing pkgs
+        pkgs=$(grep -hvE "^\s*#|^\s*$" "$@")
+        if [[ -n "$pkgs" ]]; then
+            echo -e "following packages will be installed ...\n$pkgs"
+            sudo pacman -Syu --needed $pkgs && echo "installation successfully"; return 0
+            echo "installation failed"
+        else
+            echo "pkg files are empty; abort installation"
+        fi
+    fi
+    return 1
+}
+
+# installs packages with flatpak
+inst_flatpak_pkgs() {
+    echo "installaing packages via Flatpak"
+    if check_pkgs "$@"; then # checking if pkgs file is existing
+        # installing pkgs
+        pkgs=$(grep -hvE "^\s*#|^\s*$" "$@")
+        if [[ -n "$pkgs" ]]; then
+            echo -e "following packages will be installed ...\n$pkgs"
+            for pkg in "$pkgs"; do
+                flatpak install flathub "$pkg" && echo "installation of *$pkg* successfully"; return 0
+                echo "installation failed"
+            done
+        else
+            echo "pkg files are empty; abort installation"
+        fi
+    fi
+    return 1
+}
+
+# installation packages
+inst_packages() {
+    echo "installing requirements with Pacman, Yay & Flatpak"
+    inst_pacman_pkgs $nvidia_pkgs $pacman_pkgs
+    inst_yay_pkgs $yay_pkgs
+    inst_flatpak_pkgs $flatpak_pkgs
+}
+
+# loading kernel modules
+load_modules() {
+    echo "loading modules file *$modules_list* ..."
+    if [[ -f "$modules_list" ]]; then
+    kernel_modules=$(grep -hvE "^\s*#|^\s*$" "$modules_list")
+        if [[ -n "$kernel_modules" ]]; then
+            echo "$kernel_modules"
+            sudo modprobe "$kernel_modules"
+
+        else
+            echo "*$modules_list* is empty"
+        fi
+    else
+        echo "*$modules_list* not found"
+    fi
+    return 1
+}
+
+# postgresql
+psql_setup() {
+    echo "PostgreSQL setup was ..."
+    if sudo su postgres -c "initdb -D /var/lib/postgres/data" && sudo systemctl enable --now postgresql; then
+        echo "successful"
+    else
+        echo "unsuccessful"
+    fi
+}
+
+inst_packages && load_modules # installing & loading modules
+psql_setup # setup postgresql
---
+++ src/packages/yay/pkgs.sh
@@ -0,0 +1,7 @@
+# applications
+visual-studio-code-bin
+android-studio
+vesktop-bin
+# tablet driver & osu
+opentabletdriver
+osu-lazer-bin
